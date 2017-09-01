54aa7ce3f7f85ead97eba3e603e3eea4cf2e217a
scjeffries <scott@teamgauges.com>
2017-09-01 13:57:47 -0400

    Emit newMessage on createMessage.

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Kim',
-        text: 'Hey. This is from me.'
-    });
 });
 
 socket.on('newMessage', function(message) {
