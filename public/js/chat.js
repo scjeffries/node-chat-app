aa5146f33f30f3ad08bae7226f26c85f2eb886c4
scjeffries <scott@teamgauges.com>
2017-09-11 18:34:06 -0400

    Send messages to only people in same room.

--- public/js/chat.js
+++ public/js/chat.js
@@ -88,8 +88,7 @@ jQuery('#message-form').on('submit', function(e) {
 
     const messageTextbox = jQuery('[name=message]');
 
-    socket.emit('createMessage', {
-        from: 'User',
+    socket.emit('createMessage', {        
         text: messageTextbox.val()
     }, function() {
         messageTextbox.val('');
